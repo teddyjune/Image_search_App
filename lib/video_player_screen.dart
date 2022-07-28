83097f15b80d36e5309b96d41f665c2260bda7b7
yeonjoo.lee <teddyjune@naver.com>
2022-07-28 16:12:00 +0900

    add: 동영상 재생화면 가운데에 플레이 아이콘 추가

--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -40,22 +40,31 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
             if (snapshot.connectionState == ConnectionState.done) {
               // 만약 VideoPlayerController 초기화가 끝나면, 제공된 데이터를 사용하여
               // VideoPlayer의 종횡비를 제한하세요.
-              return AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                // 영상을 보여주기 위해 VideoPlayer 위젯을 사용합니다.
-                child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        // 영상이 재생 중이라면, 일시 중지 시킵니다.
-                        if (_controller.value.isPlaying) {
-                          _controller.pause();
-                        } else {
-                          // 만약 영상이 일시 중지 상태였다면, 재생합니다.
-                          _controller.play();
-                        }
-                      });
-                    },
-                    child: VideoPlayer(_controller)),
+              return Stack(
+                alignment: Alignment.center,
+                children: [
+                  AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // 영상을 보여주기 위해 VideoPlayer 위젯을 사용합니다.
+                    child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            // 영상이 재생 중이라면, 일시 중지 시킵니다.
+                            if (_controller.value.isPlaying) {
+                              _controller.pause();
+                            } else {
+                              // 만약 영상이 일시 중지 상태였다면, 재생합니다.
+                              _controller.play();
+                            }
+                          });
+                        },
+                        child: VideoPlayer(_controller)),
+                  ),
+                  const Icon(
+                    Icons.play_circle,
+                    size: 80,
+                  ),
+                ],
               );
             } else {
               // 만약 VideoPlayerController가 여전히 초기화 중이라면,
