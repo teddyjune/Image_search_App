7552080ff543777c383608230ff9363fab7ce39a
yeonjoo.lee <teddyjune@naver.com>
2022-08-26 12:07:36 +0900

    add: viewModel test 작성

--- lib/data/model/photo.dart
+++ lib/data/model/photo.dart
@@ -15,20 +15,3 @@ class Photo with _$Photo {
 }
 
 
-// import 'package:json_annotation/json_annotation.dart';
-//
-// part 'photo.g.dart';
-//
-// @JsonSerializable(explicitToJson: true)
-// class Photo {
-//   final
-//   final
-//
-//   Photo({
-//
-//   });
-//
-//   factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
-//
-//   Map<String, dynamic> toJson() => _$PhotoToJson(this);
-// }
--- lib/ui/image_search/image_search_view_model.dart
+++ lib/ui/image_search/image_search_view_model.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:image_search_app/data/model/photo.dart';
 import 'package:image_search_app/data/repository/photo_repository.dart';
+import 'package:image_search_app/ui/image_search/main_action.dart';
+import 'package:image_search_app/ui/main_state.dart';
 
 class ImageSearchViewModel extends ChangeNotifier {
   //데이터 저장소
-  final _photoRepository = PhotoRepository();
+  late final PhotoRepository _photoRepository;
+  final MainState _state = const MainState();
 
-  //데이터
+  MainState get state => _state; //데이터
+
   List<Photo> photos = [];
 
   //로딩
   bool isLoading = false;
 
-  void fetchImages(String query) async {
+  void onAction(MainAction action) {
+    action.when(
+      getImages: (query) {
+        fetchImages(query);
+      },
+    );
+  }
 
+  void fetchImages(String query) async {
     isLoading = true;
     notifyListeners();
 
