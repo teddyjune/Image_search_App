e33d3d6c6bfb188963645557d6cb981439eba004
yeonjoo.lee <teddyjune@naver.com>
2022-08-31 17:03:50 +0900

    Result로 감싼 뒤 test 에러발생

--- test/ui/image_search/image_search_view_model_test.dart
+++ test/ui/image_search/image_search_view_model_test.dart
@@ -1,37 +1,38 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:image_search_app/data/model/photo.dart';
-// import 'package:image_search_app/data/repository/photo_repository.dart';
-// import 'package:image_search_app/ui/image_search/image_search_view_model.dart';
-// import 'package:image_search_app/ui/image_search/main_action.dart';
-//
-// void main() {
-//   test('이미지를 가져오는 기능', () async {
-//     final viewModel =
-//         ImageSearchViewModel(photoRepository: MockPhotoRepository());
-//     viewModel.onAction(const MainAction.getImages('query'));
-//     await Future.delayed(const Duration(seconds: 2));
-//
-//     expect(viewModel.state.photos.length, 3);
-//     expect(viewModel.state.photos.first.tags, 'google');
-//   });
-// }
-//
-// class MockPhotoRepository implements PhotoRepository  {
-//   @override
-//   Future<List<Photo>> getImages(String query) async {
-//     return const [
-//       Photo(
-//         previewURL: "http://www.google.com",
-//         tags: 'google',
-//       ),
-//       Photo(
-//         previewURL: "http://www.facebook.com",
-//         tags: 'facebook',
-//       ),
-//       Photo(
-//         previewURL: "http://www.kakao.com",
-//         tags: 'kakao',
-//       ),
-//     ];
-//   }
-// }
+import 'package:flutter_test/flutter_test.dart';
+import 'package:image_search_app/data/model/photo.dart';
+import 'package:image_search_app/data/repository/photo_repository.dart';
+import 'package:image_search_app/data/util/result.dart';
+import 'package:image_search_app/ui/image_search/image_search_view_model.dart';
+import 'package:image_search_app/ui/image_search/main_action.dart';
+
+void main() {
+  test('이미지를 가져오는 기능', () async {
+    final viewModel =
+        ImageSearchViewModel(photoRepository: MockPhotoRepository());
+    viewModel.onAction(const MainAction.getImages('query'));
+    await Future.delayed(const Duration(seconds: 2));
+
+    expect(viewModel.state.photos.length, 3);
+    expect(viewModel.state.photos.first.tags, 'google');
+  });
+}
+
+class MockPhotoRepository implements PhotoRepository  {
+  @override
+  Future<Result<List<Photo>>> getImages(String query) async {
+    return const [
+      Photo(
+        previewURL: "http://www.google.com",
+        tags: 'google',
+      ),
+      Photo(
+        previewURL: "http://www.facebook.com",
+        tags: 'facebook',
+      ),
+      Photo(
+        previewURL: "http://www.kakao.com",
+        tags: 'kakao',
+      ),
+    ];
+  }
+}
